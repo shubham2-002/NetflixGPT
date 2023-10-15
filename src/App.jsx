ade390ea77e23cc18107e6364d8bc7e14c9b0d7b
Shubham Umb <shubham24.8@yahoo.com>
2023-10-15 22:21:25 +0530

    Intalized Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-3xl text-red-600'>Heloo</h1>
+    </div>
+     
+  )
+}
+
+export default App
